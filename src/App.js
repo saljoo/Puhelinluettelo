0955f9f467c87abb9f2a73fd27de382797344a1b
Joonas Salminen <joonas.ej.salminen@gmail.com>
2023-02-27 13:52:12 +0200

    step8 ready

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
+import personService from './services/persons';
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
@@ -11,10 +11,10 @@ const App = () => {
 
   //Effect hook to get the data from the server
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+        .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -41,16 +41,15 @@ const App = () => {
         name: newName, number: newNumber
       }
 
-      axios
-        .post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      personService
+        .create(personObject)
+          .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
           //Clear input boxes
           setNewName('')
           setNewNumber('')
         })
     }
-    
   }
 
   //Set filterValue to be the value of the input box
